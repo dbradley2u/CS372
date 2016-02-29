70223e5420087198b0c93123d1d8f3db85195a14
Denise <porenn4u@yahoo.com>
2016-02-28 22:32:38 -0500

    Update ComposeViewController.swift

--- ComposeViewController.swift
+++ ComposeViewController.swift
@@ -7,24 +7,59 @@
 //
 
 import UIKit
+import Accounts
+import Social
 
-class ComposeViewController: UIViewController {
+class ComposeViewController: UIViewController, UITextViewDelegate {
     
+    var selectedAccount : ACAccount!
+        
     @IBOutlet weak var tweetContent: UITextView!
     @IBOutlet weak var postButton: UIButton!
     @IBOutlet weak var postActivity: UIActivityIndicatorView!
     
     @IBAction func dismissView(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
     }
     
     @IBAction func postToTwitter(sender: AnyObject) {
+        postContent(self.tweetContent.text)
     }
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.tweetContent.delegate = self
+    }
+    
+    func postContent(post : String) {
+        postActivity.startAnimating()
+        
+        if let account = selectedAccount {
+            let requestURL = NSURL(string: "https://api.twitter.com/1.1/statuses/update.json")
+            let request = SLRequest(forServiceType: SLServiceTypeTwitter,
+                requestMethod: SLRequestMethod.POST,
+                URL: requestURL,
+                parameters: NSDictionary(object: post, forKey: "status") as [NSObject : AnyObject])
+            
+            request.account = account
+            
+            request.performRequestWithHandler() {
+                responseData, urlResponse, error in
+                
+                if(urlResponse.statusCode == 200)
+                {
+                    print("Status Posted")
+                    
+                    dispatch_async(dispatch_get_main_queue())
+                        {
+                            self.postActivity.stopAnimating()
+                            self.dismissViewControllerAnimated(true, completion: nil)
+                    }
+                }
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
