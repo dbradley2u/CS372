32a07484afc6f1f0cca7aed506545c7faed51a39
Denise <porenn4u@yahoo.com>
2016-02-28 21:31:19 -0500

    Update TweetViewController.swift

--- TweetViewController.swift
+++ TweetViewController.swift
@@ -15,13 +15,26 @@ class TweetViewController: UIViewController {
     @IBOutlet weak var tweetAuthorName: UILabel!
     @IBOutlet weak var tweetText: UITextView!
     @IBAction func dismissView(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
     }
     
-
+    var selectedTweet : NSDictionary?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let userData = selectedTweet?.objectForKey("user") as! NSDictionary
+        
+        tweetText.text? = selectedTweet?.objectForKey("text") as! String
+        tweetAuthorName.text? = userData.objectForKey("name") as! String
+        
+        let imageURLString = userData.objectForKey("profile_image_url") as! String
+        let imageURL = NSURL(string: imageURLString) as NSURL!
+        let imageData = NSData(contentsOfURL: imageURL!) as NSData!
+        
+        dispatch_async(dispatch_get_main_queue()) {
+            self.tweetAuthorAvatar.image = UIImage(data: imageData!)
+        }
     }
 
     override func didReceiveMemoryWarning() {
